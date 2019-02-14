fe45af2d4c95b4c063818673def43ff276af8a9e
michaelcheco21 <michaelcheco@whsat.org>
2019-02-14 23:22:35 +0000

    Using for loops

--- js/script.js
+++ js/script.js
@@ -3,12 +3,12 @@
 // BELOW Update the songs array with four of your favorites songs.
 var songs = ["No More", "The End is Where We Begin", "Blow Me Way", "For the Glory"];
 // BELOW Add 4 More arrays to store images_links, artists, song lengths, and links for each song
-var image_links = ["https://i.ytimg.com/vi/uUmdVqHZGFE/maxresdefault.jpg",
+var imageLinks = ["https://i.ytimg.com/vi/uUmdVqHZGFE/maxresdefault.jpg",
 "https://i.ytimg.com/vi/ifLtbrk4Vg4/maxresdefault.jpg",
 "https://m.media-amazon.com/images/M/MV5BZDE0N2E4ODctYThiNS00ZTFlLTlkYTMtYTg5YzJjODM1Zjg5XkEyXkFqcGdeQXVyMzM4MjM0Nzg@._V1_.jpg",
 "https://i.ytimg.com/vi/ESwLsdCTCCo/maxresdefault.jpg"];
 var artists = ["Disturbed", "Thousand Foot Krutch", "Breaking Benjamin", "All Good Things"];
-var song_lengths = ["3:53", "3:44", "3:25", "4:46"];
+var songLengths = ["3:53", "3:44", "3:25", "4:46"];
 var links = ["https://www.youtube.com/watch?v=eQji1bEXwc0", "https://www.youtube.com/watch?v=IPQ7mvL5_04",
 "https://www.youtube.com/watch?v=L1VIh_lEP_o", "https://www.youtube.com/watch?v=zBUx6zTxr98"];
 // Make sure they match the same order as your array above
@@ -17,9 +17,27 @@ var links = ["https://www.youtube.com/watch?v=eQji1bEXwc0", "https://www.youtube
 
 function displaySongInfo(){
     // BELOW Use forEach Loop to display the data from each of your arrays in the correct div
-songs.forEach(function(songItem){
-    $("#songs").append();
-});
+ songs.forEach(function(songItem){
+    $("#songs").append("<p>" + songItem +"</p>" + "<br>");
+ });
+
+ imageLinks.forEach(function(image){
+     $("#images").append("<img src=" + image + ">" + "<br>");
+ });
+
+ artists.forEach(function(artist){
+     $("#artists").append("<p>" + artist + "</p>" + "<br>");
+ });
+
+ songLengths.forEach(function(length){
+     $("#lengths").append("<p>" + length + "</p>" + "<br>");
+ });
+
+ links.forEach(function(link){
+    $("#links").append("<a href=" + link + ">" + link + "</a>" + "<br>");
+ });
+
+}
 
 function emptySongInfo(){
     $("#songs").empty();
@@ -42,4 +60,3 @@ $("#add").click(function() {
 });
 
 displaySongInfo();
-}
